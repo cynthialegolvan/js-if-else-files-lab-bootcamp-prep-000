4201a8accafb6aebacc83c3639967c9745e8fb81
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-08 15:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ var height = "70"
 // Don't worry about this
 module.exports = { name, height
 }
-if(name==="Susan") {
-  consule.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
