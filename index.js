8f33eb6826951b439a591fb8f70d51bf1e73b31d
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-08 16:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ if (name === "Susan") {
 } else {
   console.log("The name is correct")
 }
-if (height === )
+if (height === "70") {
+  console.log("This is not correct")
+} else {
+  console.log("The height is correct")
+}
 // Don't worry about this
 module.exports = { name, height
 }
