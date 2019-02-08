f3f14948a79d0347fad226a0357151bf5b5aa79f
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-08 16:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var name = "Joe"
 var height = "74"
 if (name === "Susan") {
-  console.log("The ")
+  console.log("This is not correct ")
+} else {
+  console.log("The name is correct")
 }
+if (height === )
 // Don't worry about this
 module.exports = { name, height
 }
